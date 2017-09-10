cb9a051248d661c8a629a287b491ea3e9ecbd34e
Emmi Siitonen <emmi.siitonen@futurice.com>
2017-09-10 21:01:41 +0300

    Removed old lines from old project

--- Main.hs
+++ Main.hs
@@ -11,48 +11,12 @@ import System.Random (randomIO)
 filePath :: FilePath
 filePath = "words.txt"
 
--- data Point = Point (Int, Int) deriving (Show, Eq)
---
--- data Board = Board {
---   points :: [[(Int, Int)]]
--- } deriving (Show, Eq)
 
 data State = State {
   wordToGuess :: String,
   guessCount :: Int
 }
 
-
--- generateNewBoard :: State -> State
--- generateNewBoard state =
---   let locations = map (\x -> (map (\y -> (x,y)) [0,1..9])) [0,1,2]
---   in state { board = Board locations }
---
--- -- printBoard :: Board -> IO()
--- printBoard board =
---   -- let characters = foldl (\acc row -> acc ++ (foldl (\acc_ point -> acc_ ++ printCharacter point) acc row)) [] (points board)
---   -- let characters = map (\acc_ row -> printRow row) (points board)
---     --   characters2 = foldl (\acc_ point -> acc_ ++ printCharacter point) [] ()
---   -- let characters = map (map printCharacter) (points board)
---   --     characters2 = foldl (\acc point -> acc ++ point) [] characters
---   --   -- map (map change) $ itrCol xs
---   --
---   --   in printBoard2 characters2
---   -- in putStrLn "Böö"
-
--- printBoard2 :: [String] -> IO()
--- printBoard2 board = putStrLn board
-
--- printRow :: [(Int, Int)] -> IO()
--- printRow row =
---   print "tylos"
-
--- printCharacter :: (Int, Int) -> String
--- printCharacter point
---  | snd point == 9 = " *\n"
---  | fst point == 0 || fst point == 2 = " * "
---  | snd point == 0  = " x "
---  | otherwise = "   "
 
 gameLoop :: State -> IO()
 gameLoop gameState = do
@@ -74,17 +38,6 @@ newGame filename = do
 loop = do
   line <- getLine
   case line of
-    -- "newBoard" -> do
-    --   putStrLn "Luodaan uusi kenttä"
-    --   let updatedState = generateNewBoard state
-    --       -- board > board updatedState
-    --   print (board updatedState)
-    --   loop
-    -- "print" -> do
-    --   putStrLn "Tulostetaan kenttä"
-    --   printBoard (board state)
-    --   putStrLn "tulostettu"
-    --   loop state
     "new" -> do
         putStrLn "Aloitetaan uusi peli"
         newGame filePath
@@ -96,22 +49,3 @@ loop = do
       loop
 
 main = loop
-
-
-
-
----
---   x  x  x  x  x  x  x  x  x  x  x  x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  1  2  3  4  5  6  7  8  9  10 x
---   x  x  x  x  x  x  x  x  x  x  x  x
-
-----
